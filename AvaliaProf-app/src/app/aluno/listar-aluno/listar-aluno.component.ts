3a6c0624d989bea38102f96af31edeb373b29609
Ricardo França <ricardofs547@live.com>
2023-05-25 01:27:44 -0300

    adicionado algumas estilizações básicas

--- AvaliaProf-app/src/app/aluno/listar-aluno/listar-aluno.component.ts
+++ AvaliaProf-app/src/app/aluno/listar-aluno/listar-aluno.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ALUNOS } from 'src/app/shared/models/ALUNOS';
+import { Aluno } from 'src/app/shared/models/aluno';
+import { AlunoService } from 'src/app/shared/services/aluno/aluno.service';
 
 @Component({
   selector: 'app-listar-aluno',
   templateUrl: './listar-aluno.component.html',
   styleUrls: ['./listar-aluno.component.css']
 })
-export class ListarAlunoComponent {
+export class ListarAlunoComponent implements OnInit{
+  alunos: Aluno[] = [];
 
+  constructor(private alunoService: AlunoService) {
+
+  }
+
+  ngOnInit(): void {
+     this.alunoService.listar().subscribe(alunosRetornados => this.alunos = alunosRetornados)
+  }
 }
