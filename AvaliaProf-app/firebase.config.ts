14e2b4263ac699fca1c4c2603ed9ac7ea550012a
Jonas A <jonasapm94@gmail.com>
2023-06-08 16:09:52 -0300

    firebase first commit

---
+++ AvaliaProf-app/firebase.config.ts
@@ -0,0 +1,9 @@
+export const FirebaseConfig = {
+  apiKey: "AIzaSyCzlUxRtMFmBurrB3o1owRRhKpBiNcjyAw",
+  authDomain: "avaliaprof-62d62.firebaseapp.com",
+  projectId: "avaliaprof-62d62",
+  storageBucket: "avaliaprof-62d62.appspot.com",
+  messagingSenderId: "609782331999",
+  appId: "1:609782331999:web:c69e437164bb2e82ceb9ea",
+  measurementId: "G-8EQ7PTTJ85"
+};
---
+++ AvaliaProf-app/src/app/firestore/firestore.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AngularFireModule } from '@angular/fire';
+import { FirebaseConfig } from 'firebase.config';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+  ]
+})
+export class FirestoreModule { }
