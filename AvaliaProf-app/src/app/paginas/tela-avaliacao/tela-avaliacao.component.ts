7267d1999d005d4a0fdb916a85e8fddd47d567cd
Ricardo França <ricardofs547@live.com>
2023-06-15 14:46:02 -0300

    logica da tela-avaliacao inicializada

--- AvaliaProf-app/src/app/paginas/tela-avaliacao/tela-avaliacao.component.ts
+++ AvaliaProf-app/src/app/paginas/tela-avaliacao/tela-avaliacao.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Avaliacao } from 'src/app/shared/models/avaliacao';
+import { AvaliacaoService } from 'src/app/shared/services/avaliacao/avaliacao.service';
 
 @Component({
   selector: 'app-tela-avaliacao',
   templateUrl: './tela-avaliacao.component.html',
   styleUrls: ['./tela-avaliacao.component.css']
 })
-export class TelaAvaliacaoComponent {
 
+export class TelaAvaliacaoComponent {
+  avaliacao: Avaliacao;
+  constructor(private avaliavaoService: AvaliacaoService, private roteador: Router) {
+    this.avaliacao = new Avaliacao(0,'','','');
+  }
+
+  publicar() {
+    this.avaliavaoService.inserir(this.avaliacao).subscribe(
+      (avaliacaoRetornada) => {
+        console.log(avaliacaoRetornada);
+        this.avaliacao = new Avaliacao(0,'','','');
+        this.roteador.navigate(['/'])
+      }
+    )
+  }
 }
